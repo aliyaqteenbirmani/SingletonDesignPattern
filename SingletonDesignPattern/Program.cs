12f06294aa74647d18f92423bc3ea79c1df21fae
Ali Yaqteen <aliyaqteenbirmani512@gmail.com>
2025-01-12 21:31:02 +0500

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace SingletonDesignPattern
+{
+    public class Program
+    {
+        static void AccessSingleton()
+        {
+            SingletonClass singleton = SingletonClass.GetInstance();
+            singleton.PrintMessage ($" Thread {Thread.CurrentThread.ManagedThreadId}");
+        }
+        static void Main(string[] args)
+        {
+
+            SingletonClass singleton1 = SingletonClass.GetInstance();
+            Console.WriteLine(singleton1.GetHashCode());
+
+            SingletonClass singleton2 = SingletonClass.GetInstance();
+            Console.WriteLine(singleton2.GetHashCode());
+
+            SingletonClass singleton3 = SingletonClass.GetInstance();
+            Console.WriteLine(singleton3.GetHashCode());
+
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(AccessSingleton);
+                threads[i].Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("All threads have completed successfully");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonDesignPattern/SingletonClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    // for sibgleton design pattern class must be sealded
+    sealed class SingletonClass
+    {
+        // using Lazy<T> for thread-safe and lazy initialization to make sure that each thread is getting the same instance
+        private static readonly Lazy<SingletonClass> _instance = new Lazy<SingletonClass>(() => new SingletonClass());
+
+        // constructor must be private to prevent from creating multiple instances
+        private SingletonClass() { }
+
+        // static globale-point access of instance
+        public static SingletonClass GetInstance()
+        {
+            return _instance.Value;
+        }
+
+        // Method to print the HashCode of Singleton instance
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine($"{message} from Instance {GetHashCode()}");
+        }
+    }
+}
