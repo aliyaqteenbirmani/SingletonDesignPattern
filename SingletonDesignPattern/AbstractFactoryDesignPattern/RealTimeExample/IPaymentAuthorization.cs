e06c2fa0a5752a94e83991b87b88b90c1a727437
Ali Yaqteen <aliyaqteenbirmani512@gmail.com>
2025-01-15 22:44:17 +0500

    Real time example of Abstract Factory Design Pattern

---
+++ SingletonDesignPattern/AbstractFactoryDesignPattern/RealTimeExample/IPaymentAuthorization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.AbstractFactoryDesignPattern.RealTimeExample
+{
+    public interface IPaymentAuthorization
+    {
+        bool AuthorizePayment(decimal amount);
+    }
+
+    public interface IPaymentTransfer
+    {
+        bool Transfer(decimal amount);
+    }
+
+
+}
